37e2b93fdc0907d29def69d9c11f78c1d1303a1e
rdnrn <rodina.arina00@gmail.com>
2022-03-15 13:00:50 +0300

    Update my_list.rb

--- my_list.rb
+++ my_list.rb
@@ -5,6 +5,17 @@ class MyList
   def initialize(*list)
     @list = list
   end
+
+  def each
+    return to_enum(:each) unless block_given?
+
+    counter = 0
+    while counter < @list.length
+      yield(@list[counter])
+      counter += 1
+    end
+    @list
+end
 end
 
 # Verify solution:
